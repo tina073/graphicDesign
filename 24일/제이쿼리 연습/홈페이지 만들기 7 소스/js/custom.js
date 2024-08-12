a9f2eac61a58d01bd7ba9efe3f1ad9cb8a6fc4ca
tina073 <164857904+tina073@users.noreply.github.com>
2024-08-12 18:01:57 +0900

    Add files via upload

---
+++ 24일/제이쿼리 연습/홈페이지 만들기 7 소스/js/custom.js
@@ -0,0 +1,65 @@
+﻿$(document).ready(function(){  	
+	//1초마다 현재 시간값을 재설정
+
+	let timer = setInterval(function(){
+		let nowTime = new Date();  //사용자의 시스템 시간을 불러움
+		let hr = nowTime.getHours();
+		let min = nowTime.getMinutes();
+		let sec = nowTime.getSeconds();
+		if(hr >= 10) {	//시간이 두 자리 숫자라면(10~23)
+			hrNum = hr;
+		} else {
+			hrNum = "0" + hr;	//시간이 한 자리 숫자라면(0~9)
+		}
+		
+		if(min >= 10) {	//분이 두 자리 숫자라면(10~59)
+			minNum = min;
+		} else {
+			minNum = "0" + min;	//분이 한 자리 숫자라면(0~9)
+		}
+		
+		if(sec >= 10) {	//초가 두 자리 숫자라면(10~59)
+			secNum = sec;
+		} else {
+			secNum = "0" + sec;	//초가 한 자리 숫자라면(0~9)
+		}
+		
+		//모바일 액정화면 안에 시간 적용
+		$("figure p span").eq(0).text(hrNum);
+		$("figure p span").eq(1).text(minNum);
+		$("figure p span").eq(2).text(secNum);
+		
+	//현재 시간에 맞춰 아래쪽 테마 메뉴와 배경 이미지,로고,글자 색상 변경
+	//morning : 5~10, afternoon : 11~15, evening : 16~19, night : 20~4
+	if(hr >= 5 && hr <= 10) {
+		$("#wrap").removeClass();
+		$("#wrap").addClass("morning");
+		$("nav li").removeClass();
+		$("nav li").eq(0).addClass("on");
+	} else if(hr >= 11 && hr <= 15) {
+		$("#wrap").removeClass();
+		$("#wrap").addClass("afternoon");
+		$("nav li").removeClass();
+		$("nav li").eq(1).addClass("on");
+	} else if(hr >= 16 && hr <= 19) {
+		$("#wrap").removeClass();
+		$("#wrap").addClass("evening");
+		$("nav li").removeClass();
+		$("nav li").eq(2).addClass("on");
+	} else {
+		$("#wrap").removeClass();
+		$("#wrap").addClass("night");
+		$("nav li").removeClass();
+		$("nav li").eq(3).addClass("on");
+	} 
+	}, 1000);	//setInterval 끝
+
+	//nav버튼 클릭시 화면 테마 변경
+	$("nav li").click(function(){
+		let className = $(this).children("a").text();	//클릭한 li의 자식인 a가 가지고 있는 글자를 가지고 옴
+		$("nav li").removeClass();
+		$(this).addClass("on");
+		$("#wrap").removeClass();
+		$("#wrap").addClass(className);
+	})
+}); // function 끝
---
+++ 24일/제이쿼리 연습/홈페이지 만들기 연습 6 소스/js/custom.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function(){   
+	//로고를 클릭하면  로고는 왼쪽 상단,address는 오른쪽 하단으로 이동
+	$("h1").click(function(){
+		$(this).stop().animate({
+			top:"-100px",
+			left:0
+		}, 1500, "easeInOutBack");
+		$("address").stop().animate({
+			bottom:"-100px",
+			right:0
+		}, 1500, "easeInOutBack");
+	
+		//빨간색 테두리를 위,오른쪽, 아래,왼쪽 방향 순서대로 그린다
+		$(".top").delay(1700).animate({width:"100%"}, 500, function(){
+			$(".right").animate({ height:"100%"}, 500, function(){
+				$(".bottom").animate({ width:"100%"}, 500, function(){
+					$(".left").animate({ height:"100%"}, 500,function(){
+						$(".box1").addClass("on");
+						$(".top, .right, .bottom, .left").delay(1700).animate({opacity:0});
+						$("#gnb").delay(1500).animate({top:"-30px", opacity:1});
+						$(".util").delay(2000).animate({top:"-150px", opacity:1});
+						$(".search").delay(2500).animate({bottom:"-50px", opacity:1});
+						$(".footer").delay(3000).animate({bottom:"-50px", opacity:1});
+					});
+				});
+			});
+		});
+	});  //click event end
+	
+	//왼쪽 상단에 있는 + 버튼을 누르면 회전하면서 두번째 article이 나타나고 첫번째 article은 사라지게 한다.
+	//이때 왼쪽 상단의 첫번째 화면의 +버튼도 크기가 10배로 커지면서 사라지고 두번째 화면의 +버튼은 크기가 1배로 작아지면서 나타나게 한다.
+	//(그 반대로도 작동하게 만든다)
+		$(".btnMenu").click(function(){
+			$("section > article").removeClass("on");
+			$(".box2").addClass("on");
+		});
+		
+		$(".btnMenu2").click(function(){
+			$("section > article").removeClass("on");
+			$(".box1").addClass("on");
+		});
+}); // document end
